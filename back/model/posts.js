8bab0b0440366cc01392ec4c09cb544534605272
João Costa <costav1402@gmail.com>
2021-07-31 14:57:32 -0300

    implementando delete e put no backend

--- back/model/posts.js
+++ back/model/posts.js
@@ -7,13 +7,23 @@ module.exports = {
             email:"costav1402@gmail.com",
             endereco:"Rua Guirareia, 389, Irajá- Rio De Janeiro"
         },
+        {
+            id:"2",
+            nomeCompleto:"Alberto Souza",
+            telefone:"(222)222-222",
+            email:"Souza@gmail.com",
+            endereco:"Rua Guiratim, 389, Irajá- Rio De Janeiro"
+        },
     ],
     getAll(){
         return this.posts;
     },
     newPost(nomeCompleto, telefone, email, endereco){
         this.posts.push({id:generateID(), nomeCompleto, telefone, email, endereco});
-    }
+    },
+    updatePost(id,nomeCompleto, telefone, email, endereco){
+        this.posts.push({id:generateID(), nomeCompleto, telefone, email, endereco});
+    },
 }
 function generateID(){
     return Math.random().toString(36).substr(2,9);
--- back/routes/api.js
+++ back/routes/api.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors')
 const router =express.Router();
 const posts = require('../model/posts');
+const post = posts.posts;
 const options = {
     origin: "http://192.168.1.65:8000"
 }
@@ -10,13 +11,39 @@ router.use(cors(options));
 router.get("/all", (req,res)=>{
     res.json(JSON.stringify(posts.getAll()));
 });
+// router.put("/all/:id", (req,res)=>{
+//     const id = req.params.id;
+//     let nomeCompleto = req.body.nomeCompleto;
+//     let telefone = req.body.telefone;
+//     let email = req.body.email;
+//     let endereco = req.body.endereco;
+//     posts.updatePost(id,nomeCompleto, telefone, email, endereco);
+//     res.send("Post atualizado com sucesso", id);
+// });
+function findItem(id){
+    return post.find(item=> item.id == id);
+}
 
+router.put('/all/:id', bodyParser.json(), (req, res) => {
+    let nomeCompleto = req.body.nomeCompleto;
+    let item = findItem(req.params.id);
+    item = { ...item, nomeCompleto: nomeCompleto};
+    return res.json(item);
+ });
+ router.delete('/all/:id', bodyParser.json(), (req, res) => {
+    let item = findItem(req.params.id);
+    return res.json(item);
+ });
+router.get('/all/:id', (req, res) => {
+    const item = findItem(req.params.id);
+    return res.json(item);
+  });
 router.post("/new", bodyParser.json() , (req,res)=>{
     let nomeCompleto = req.body.nomeCompleto;
     let telefone = req.body.telefone;
     let email = req.body.email;
     let endereco = req.body.endereco;
-    posts. newPost(nomeCompleto, telefone, email, endereco);
+    posts.newPost(nomeCompleto, telefone, email, endereco);
     res.send("Post adicionado");
 });
 
